8cc90da3157cd46d457edb5e305fc0aa769d82c9
siehun <126447514+siehun@users.noreply.github.com>
2025-07-18 08:23:03 +0800

    用户微服务配置和启动类

---
+++ y-im-platform-user/y-im-platform-user-starter/src/main/java/io/yue/im/platform/user/IMPlatformUserStarter.java
@@ -0,0 +1,23 @@
+package io.yue.im.platform.user;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@EnableDubbo
+@EnableDiscoveryClient
+@EnableAspectJAutoProxy(exposeProxy = true)
+@MapperScan(basePackages = {"io.yue.im.platform.user.domain.repository"})
+@ComponentScan(basePackages = {"io.yue.im"})
+@SpringBootApplication(exclude= {SecurityAutoConfiguration.class })// 禁用secrity
+public class IMPlatformUserStarter {
+    public static void main(String[] args) {
+        System.setProperty("user.home", "D:/y-im/y-im-platform-user");
+        SpringApplication.run(IMPlatformUserStarter.class, args);
+    }
+}
---
+++ y-im-platform-user/y-im-platform-user-starter/src/main/java/io/yue/im/platform/user/config/LongToStringConfig.java
@@ -0,0 +1,27 @@
+package io.yue.im.platform.user.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+/**
+ * @description 防止Long型数据精度丢失
+ */
+@Configuration
+public class LongToStringConfig {
+    @Bean
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder mapperBuilder) {
+        ObjectMapper build = mapperBuilder.createXmlMapper(false).build();
+        build.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(Long.class, ToStringSerializer.instance);
+        module.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        build.registerModule(module);
+        return build;
+
+    }
+}
